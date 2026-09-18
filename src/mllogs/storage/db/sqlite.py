8249971d2061dc46ec3e7107a10537101e4c8140
Finn Walsh <fpw5@pitt.edu>
2026-09-18 16:06:03 -0400

    finish initial sqlite storage functions

--- src/mllogs/storage/db/sqlite.py
+++ src/mllogs/storage/db/sqlite.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import sqlite3
+import json
+from datetime import datetime
 
 from .base import DBStore
-from mllogs.run import Run
+from mllogs.run import Run, RunStatus, Artifact
 
 
 class SQLiteStore(DBStore):
@@ -12,6 +14,7 @@ class SQLiteStore(DBStore):
         self._db_path.parent.mkdir(parents=True, exist_ok=True)
 
         self._connection = sqlite3.connect(self._db_path)
+        self._connection.row_factory = sqlite3.Row
         self._connection.execute("PRAGMA foreign_keys = ON")
 
         self._initialize_schema()
@@ -44,7 +47,7 @@ class SQLiteStore(DBStore):
             CREATE TABLE IF NOT EXISTS metrics (
                 run_id          TEXT NOT NULL,
                 key             TEXT NOT NULL,
-                value           TEXT NOT NULL,
+                value           REAL NOT NULL,
 
                 PRIMARY KEY (run_id, key),
                 FOREIGN KEY (run_id)
@@ -55,7 +58,7 @@ class SQLiteStore(DBStore):
             CREATE TABLE IF NOT EXISTS tags (
                 run_id          TEXT NOT NULL,
                 key             TEXT NOT NULL,
-                value           REAL NOT NULL,
+                value           TEXT NOT NULL,
 
                 PRIMARY KEY (run_id, key),
                 FOREIGN KEY (run_id)
@@ -108,7 +111,7 @@ class SQLiteStore(DBStore):
                 VALUES (?, ?, ?)
                 """,
                 [
-                    (run.id, key, str(value))
+                    (run.id, key, json.dumps(value))
                     for key, value in run.params.items()
                 ],
             )
@@ -119,8 +122,8 @@ class SQLiteStore(DBStore):
                 VALUES (?, ?, ?)
                 """,
                 [
-                    (run.id, key, str(value))
-                    for key, value in run.params.items()
+                    (run.id, key, value)
+                    for key, value in run.metrics.items()
                 ],
             )
 
@@ -131,7 +134,7 @@ class SQLiteStore(DBStore):
                 """,
                 [
                     (run.id, key, str(value))
-                    for key, value in run.params.items()
+                    for key, value in run.tags.items()
                 ],
             )
 
@@ -155,3 +158,117 @@ class SQLiteStore(DBStore):
                     for artifact in run.artifacts
                 ]
             )
+
+
+    def load_run(self, run_id: str | None = None) -> Run | None:
+        if run_id is None:
+            run_row = self._connection.execute(
+                """
+                SELECT *
+                FROM runs
+                ORDER BY started_at DESC
+                LIMIT 1
+                """
+            ).fetchone()
+        else:
+            run_row = self._connection.execute(
+                "SELECT * FROM runs WHERE id = ?",
+                (run_id,),
+            ).fetchone()
+
+        if run_row is None:
+            return None
+
+        run_id = run_row["id"]
+        
+        param_rows = self._connection.execute(
+            "SELECT key, value FROM params WHERE run_id = ?",
+            (run_id,),
+        ).fetchall()
+
+        metric_rows = self._connection.execute(
+            "SELECT key, value FROM metrics WHERE run_id = ?",
+            (run_id,),
+        ).fetchall()
+
+        tag_rows = self._connection.execute(
+            "SELECT key, value FROM tags WHERE run_id = ?",
+            (run_id,),
+        ).fetchall()
+
+        artifact_rows = self._connection.execute(
+            "SELECT * FROM artifacts WHERE run_id = ?",
+            (run_id,),
+        ).fetchall()
+
+        return Run(
+            id=run_row["id"],
+            started_at=datetime.fromisoformat(run_row["started_at"]),
+            status=RunStatus(run_row["status"]),
+            name=run_row["name"],
+            run_type=run_row["run_type"],
+            ended_at=(
+                datetime.fromisoformat(run_row["ended_at"])
+                if run_row["ended_at"]
+                else None
+            ),
+            params={
+                row["key"]: json.loads(row["value"])
+                for row in param_rows
+            },
+            metrics={
+                row["key"]: row["value"]
+                for row in metric_rows
+            },
+            tags={
+                row["key"]: row["value"]
+                for row in tag_rows
+            },
+            artifacts=[
+                Artifact(
+                    name=row["name"],
+                    uri=row["uri"],
+                    artifact_type=row["artifact_type"],
+                )
+                for row in artifact_rows
+            ],
+        )
+
+
+
+    def list_runs(self, limit: int | None = None) -> list[Run]:
+        if limit is not None and limit <= 0:
+            raise ValueError("limit must be greater than 0")
+
+        query = """
+            SELECT id
+            FROM runs
+            ORDER BY started_at DESC
+        """
+
+        params = ()
+
+        if limit is not None:
+            query += " LIMIT ?"
+            params = (limit, )
+
+        rows = self._connection.execute(
+            query,
+            params,
+        ).fetchall()
+
+        return [
+            self.load_run(row["id"])
+            for row in rows
+        ]
+
+    def delete_run(self, run_id: str) -> None:
+        with self._connection:
+            cursor = self._connection.execute(
+                "DELETE FROM runs WHERE id = ?",
+                (run_id,),
+            )
+
+        if cursor.rowcount == 0:
+            raise KeyError(f"Run not found: run_id")
+    
