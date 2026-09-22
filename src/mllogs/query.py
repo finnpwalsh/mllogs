d4d20dec435a4b5e38b3e8bc840bcaae12335f77
Finn Walsh <fpw5@pitt.edu>
2026-09-22 15:03:17 -0400

    rename db store inside Query

--- src/mllogs/query.py
+++ src/mllogs/query.py
@@ -4,17 +4,17 @@ from .storage.db import DBStore
 
 
 class Query:
-    def __init__(self, db: DBStore) -> None:
-        self._db = db
+    def __init__(self, db_store: DBStore) -> None:
+        self._db_store = db_store
 
     def get_run(self, run_id: str) -> Run:
-        return self._db.load_run(run_id)
+        return self._db_store.load_run(run_id)
 
     def get_params(self, run_id: str) -> dict[str, ParamValue]:
-        return self._db.load_params(run_id)
+        return self._db_store.load_params(run_id)
 
     def get_metrics(self, run_id: str) -> dict[str, float]:
-        return self._db.load_metrics(run_id)
+        return self._db_store.load_metrics(run_id)
 
     def get_tags(self, run_id: str) -> dict[str, str]:
-        return self._db.load_tags(run_id)
+        return self._db_store.load_tags(run_id)
